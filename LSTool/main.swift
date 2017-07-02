cdd4dc336613b093290660e4e29bda47adf517a1
ViktorSimko <aqviktor@gmail.com>
2017-07-02 15:38:29 +0200

    Initial Commit

---
+++ LSTool/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LSTool
+//
+//  Created by Simkó Viktor on 2017. 07. 02..
+//  Copyright © 2017. Simkó Viktor. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
